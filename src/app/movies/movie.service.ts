6025947347103ead26662b98e318c3f0e8369a57
Amir Zare Pashaei <amir.zp90@gmail.com>
2019-02-21 13:39:06 +0200

    Final touches.

--- src/app/movies/movie.service.ts
+++ src/app/movies/movie.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
-import { IMovie} from './movie';
-import {HttpClient, HttpErrorResponse} from '@angular/common/http';
-import {Observable, throwError} from 'rxjs';
-import {catchError, map, tap} from 'rxjs/operators';
+import { IMovie } from './movie';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
